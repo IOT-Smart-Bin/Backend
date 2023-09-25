3dd809c00118333c2087872ec6f6233960480ab4
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2023-09-25 21:34:28 +0700

    Create APIs for search, bins, locations, and histories

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import databases
 import config as cfg 
+from crud import interface
+import schemas
 
 DATABASE_URL = f"postgresql://{cfg.DATABASE_USERNAME}:{cfg.DATABASE_PASSWORD}@{cfg.DATABASE_HOST}:5432/{cfg.DATABASE_NAME}"
 db = databases.Database(DATABASE_URL)
@@ -27,3 +29,19 @@ async def shutdown():
 @app.get("/")
 async def root():
     return {"message": "Unused route"}
+
+@app.post("/search")
+async def search_bins(search_criteria: schemas.SearchBin):
+    return await interface.search(db, search_criteria)
+
+@app.post("/bins")
+async def search_bins(bid_list: list[int]):
+    return await interface.bins(db, bid_list)
+
+@app.post("/locations")
+async def search_bins(bid_list: list[int]):
+    return await interface.locations(db, bid_list)
+
+@app.post("/histories")
+async def search_bins(bin_info: schemas.GetBinHistories):
+    return await interface.histories(db, bin_info)
