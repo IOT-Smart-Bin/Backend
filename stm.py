3c3b48589b7593781f4ed2debd458938e23f611b
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2023-09-30 19:42:37 +0700

    Socket handling with threading

--- stm.py
+++ stm.py
@@ -1,70 +1,58 @@
 import socket
+import threading
 
 HOST = "127.0.0.1"
-PORT = 5432
+PORT = 5678
+
+
+def handle_client(client_socket, client_address):
+    try:
+        while True:
+            request = client_socket.recv(1024)
+
+            # TL;DR: Send "close" before termination connection.
+
+            # This catches if the client or server unexpectedly closes the socket connection, exit the loop
+            # To avoid this happening, make sure that the client and server are in sync on when to close the connection.
+            # If we want to avoid "[WinError 10053] An established connection was aborted by the software in your host machine," use the code below
+
+            # if not request:
+            #     break
+
+            request = request.decode("utf-8")
+
+            if request.lower() == "close":
+                client_socket.send("closed".encode("utf-8"))
+                break
+
+            print(f"Received from {client_address}: {request}")
+
+            response = "accepted".encode("utf-8")
+            client_socket.send(response)
+    except ConnectionResetError:
+        print(f"Connection to {client_address} reset by the client.")
+    except Exception as e:
+        print(f"An error occurred with Client {client_address}: {str(e)}")
+    finally:
+        client_socket.close()
+        print(f"Connection to {client_address} closed")
 
 
 def run_server():
-    # Create a dictionary to store client information
-    clients = {}
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen(5)
+    print(f"Listening on {HOST}:{PORT}")
 
     while True:
-        # Create a socket object
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        # Bind the socket to a specific address and port
-        server.bind((HOST, PORT))
-        # Listen for incoming connections
-        server.listen(0)
-        print(f"Listening on {HOST}:{PORT}")
-
-        # Accept incoming connections
         client_socket, client_address = server.accept()
         print(
             f"Accepted connection from {client_address[0]}:{client_address[1]}")
 
-        # Store client information in the dictionary
-        client_id = len(clients) + 1
-        clients[client_id] = {
-            "socket": client_socket, "address": client_address}
-
-        try:
-            # Receive data from the client
-            while True:
-                request = client_socket.recv(1024)
-
-                # This catches if the client or server unexpectedly closes the socket connection, exit the loop
-                # To avoid this happening, make sure that the client and server are in sync regarding when to close the connection.
-                # if not request:
-                #     break
-
-                request = request.decode("utf-8")  # Convert bytes to string
-
-                # If we receive "close" from the client, then we break
-                # out of the loop and close the connection
-                if request.lower() == "close":
-                    # Send response to the client which acknowledges that the
-                    # connection should be closed and break out of the loop
-                    client_socket.send("closed".encode("utf-8"))
-                    break
-
-                print(f"Received from Client {client_id}: {request}")
-
-                response = "accepted".encode(
-                    "utf-8")  # Convert string to bytes
-                # Convert and send accept response to the client
-                client_socket.send(response)
-        except ConnectionResetError:
-            print(f"Connection to Client {client_id} reset by the client.")
-        except Exception as e:
-            print(f"An error occurred with Client {client_id}: {str(e)}")
-        finally:
-            # Close connection socket with the client
-            client_socket.close()
-            print(f"Connection to Client {client_id} closed")
-            # Remove the client information from the dictionary
-            del clients[client_id]
-            # Don't close the server socket here; it should keep listening for new connections
+        # Create a new thread to handle the client
+        client_thread = threading.Thread(
+            target=handle_client, args=(client_socket, client_address))
+        client_thread.start()
 
 
 if __name__ == "__main__":
